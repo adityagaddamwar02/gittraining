9e085456486a49517d948a9ae745f0b79b54d9c1
adityagaddamwar02 <adityagaddamwar02@gmail.com>
2018-03-29 15:50:04 -0500

    second

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from first_app import views
-
-urlpatterns = [
-        path('welcome/',views.welcome),
-        path('hello/',views.hello),
-        path('img/',views.img)
-        ]
-
